296cc677df8a32b15e462695f0cefc79fe170257
Pratyaksh Gupta <pincodeazm@gmail.com>
2025-07-19 12:24:27 +0530

    added delivery and pincode options in cart page

--- app/(shop)/cart/page.tsx
+++ app/(shop)/cart/page.tsx
@@ -1,33 +1,110 @@
 "use client";
 
+import useAmountStore from "@/app/stores/useAmountStore";
 import useCartStore from "@/app/stores/useCartStore";
+import useDeliveryStore from "@/app/stores/useDeliveryStore";
+import { Input } from "@/components/ui/input";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BsHandbag } from "react-icons/bs";
 import { LuMinus, LuPlus } from "react-icons/lu";
 import { toast } from "sonner";
 
 const CartPage = () => {
   const { cart, removeFromCart, updateQuantity } = useCartStore();
+
+  const {
+    pincode,
+    deliveryCharge,
+    paymentMethod,
+    setPincode,
+    setDeliveryCharge,
+    setPaymentMethod,
+  } = useDeliveryStore();
+
+  const {
+    subTotal,
+    discount,
+    totalAmount,
+    setSubTotal,
+    setDiscount,
+    setTotalAmount,
+  } = useAmountStore();
+
   const [promoCode, setPromoCode] = useState<string>("");
+  const [deliveryPossible, setDeliveryPossible] = useState<boolean>(false);
   const session = useSession();
 
-  const subTotal =
-    cart.length > 0
-      ? cart.reduce(
-          (acc, product) => acc + product.price.mrp * product.quantity,
-          0
-        )
-      : 0;
+  //cart actions
+  useEffect(() => {
+    if (cart.length > 0) {
+      const cartSubTotal = cart.reduce(
+        (acc, product) => acc + product.price.mrp * product.quantity,
+        0
+      );
+      setSubTotal(cartSubTotal);
+    }
 
-  const discount =
-    subTotal -
-    cart.reduce(
-      (acc, product) => acc + product.price.sellingPrice * product.quantity,
-      0
-    );
+    const discount =
+      subTotal -
+      cart.reduce(
+        (acc, product) => acc + product.price.sellingPrice * product.quantity,
+        0
+      );
+    setDiscount(discount);
+
+    const totalAmt =
+      subTotal - discount + (deliveryCharge ? deliveryCharge : 0);
+    setTotalAmount(totalAmt);
+  }, [
+    cart,
+    subTotal,
+    deliveryCharge,
+    setSubTotal,
+    setDiscount,
+    setTotalAmount,
+  ]);
+
+  // function to find serviceable pincode and their shipping costs
+  useEffect(() => {
+    const findPincode = async () => {
+      //api to fetch if delivery is possible at a pincode
+      const pincodeServiceable = await fetch(
+        `/api/delhivery/pincode-check/?pincode=${pincode}`
+      );
+      const deliveryAvailable = await pincodeServiceable.json();
+      if (deliveryAvailable.delivery_codes.length == 0) {
+        setDeliveryPossible(false);
+        return;
+      }
+
+      setDeliveryPossible(true);
+    };
+
+    if (pincode.length === 6) {
+      findPincode();
+    } else {
+      setDeliveryPossible(false);
+    }
+  }, [pincode]);
+
+  //function to calculate shipping cost to a pincode
+  useEffect(() => {
+    const handleDelivery = async () => {
+      const shippingCostResponse = await fetch(
+        `/api/delhivery/shipping-cost/?pincode=${pincode}&paymentMode=${paymentMethod}`
+      );
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const shippingCostData: any = await shippingCostResponse.json();
+      console.log("shipping cost", shippingCostData);
+      setDeliveryCharge(shippingCostData[0]?.total_amount);
+    };
+
+    handleDelivery();
+  }, [paymentMethod, setDeliveryCharge, pincode]);
 
   const handleUpdateCart = async () => {
     if (session.data?.user) {
@@ -76,9 +153,7 @@ const CartPage = () => {
             <span className="text-xl lg:text-3xl font-medium">
               Cart Summary
             </span>
-
             {/* Image section to display product and quantity */}
-
             {cart.map((product) => (
               <div
                 key={product?._id?.toString()}
@@ -159,6 +234,14 @@ const CartPage = () => {
                 </div>
               </div>
             ))}
+            <div className="w-full flex items-end justify-end">
+              <Link
+                href="/checkout"
+                className="bg-darkTeal px-5 py-2 text-ivory rounded-full"
+              >
+                Proceed to Checkout
+              </Link>
+            </div>
           </div>
 
           {/* Subtotal Section */}
@@ -170,23 +253,17 @@ const CartPage = () => {
               </li>
               <li>
                 <p>Discount</p>
-                <p>- ₹ {discount}</p>
+                <p className="text-green-600">- ₹ {discount}</p>
               </li>
               <li>
                 <p>Delivery</p>
-                <p>₹ 999</p>
-              </li>
-              <li>
-                <p>Taxes</p>
-                <p>₹ 999</p>
+                <p className="">₹ {deliveryCharge}</p>
               </li>
             </ul>
-
             <div className="flex items-center justify-between font-semibold py-4 border-t border-b border-dotted border-neutral-500">
               <span>Total</span>
-              <span>Rs. {subTotal - discount}</span>
+              <span>Rs. {totalAmount}</span>
             </div>
-
             <div className="flex flex-col gap-2 pt-5">
               <span className="text-sm">Do you have a promotional code?</span>
               <div className="flex bg-gray-200 rounded-full">
@@ -210,6 +287,81 @@ const CartPage = () => {
 
               {promoCode}
             </div>
+            <div className="pt-4">
+              <span className="text-sm">
+                Enter pincode to check delivery options
+              </span>
+              <div className="flex gap-5">
+                <Input
+                  className="bg-white"
+                  placeholder="enter pincode"
+                  defaultValue={pincode}
+                  onChange={(e) => {
+                    setTimeout(() => {
+                      setPincode(e.target.value);
+                    }, 1000);
+                  }}
+                />
+              </div>
+            </div>
+            {pincode.length > 0 && pincode.length !== 6 && (
+              <>
+                <span className="text-red-500 text-sm">
+                  Error : please enter 6 digit pincode
+                </span>
+              </>
+            )}
+
+            {pincode.length === 6 && (
+              <>
+                {deliveryPossible ? (
+                  <div className="text-green-600 font-semibold text-sm">
+                    Delivery in 4-7 business days
+                  </div>
+                ) : (
+                  <div className="text-red-400 font-semibold text-sm">
+                    delivery not possible
+                  </div>
+                )}
+              </>
+            )}
+
+            {pincode.length === 6 && deliveryPossible && (
+              <div className="pt-4 flex flex-col gap-2">
+                <span className="text-sm">Choose Payment Method</span>
+
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center space-x-2 bg-neutral-100 p-2 rounded-md">
+                    <input
+                      type="radio"
+                      id="COD"
+                      value="COD"
+                      name="payment-method"
+                      defaultChecked
+                      onChange={(e) => setPaymentMethod(e.target.value)}
+                    />
+                    <label htmlFor="COD">Cash On Delivery</label>
+                  </div>
+
+                  <div className="flex items-center space-x-2 bg-neutral-100 p-2 rounded-md">
+                    <input
+                      type="radio"
+                      id="Pre-paid"
+                      value="Pre-paid"
+                      name="payment-method"
+                      onChange={(e) => setPaymentMethod(e.target.value)}
+                    />
+                    <label htmlFor="Pre-paid">
+                      Pre-paid (UPI / Net Banking / Card)
+                    </label>
+                  </div>
+                </div>
+
+                <span className="text-red-500 text-sm font-medium">
+                  Note: COD might incur additional delivery charges
+                </span>
+              </div>
+            )}
           </div>
         </div>
       )}
