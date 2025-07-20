afb0ca9f6ac320b78e53ffd411f3736f7fceb51e
Pratyaksh Gupta <pincodeazm@gmail.com>
2025-07-21 01:37:21 +0530

    uploaded carousel images

--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -8,13 +8,15 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 
-import img1 from "@/public/products/product 1.jpg";
-import img2 from "@/public/products/product 2.jpg";
-import img3 from "@/public/products/product 3.jpg";
-import { useEffect, useState } from "react";
+import img1 from "@/public/images/M-1.jpg";
+import img2 from "@/public/images/M-2.jpg";
+import img3 from "@/public/images/M-3.jpg";
+import img4 from "@/public/images/M-4.jpg";
+import img5 from "@/public/images/M-5.jpg";
+
 import Autoplay from "embla-carousel-autoplay";
 import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
-import { FaArrowRight } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
   const [api, setApi] = useState<CarouselApi>();
@@ -53,16 +55,12 @@ const Hero = () => {
           className="w-full aspect-square lg:aspect-[2/1] max-h-[80vh] bg-neutral-300 rounded-lg lg:rounded-4xl overflow-hidden"
         >
           <CarouselContent className="w-full h-full -ml-0 *:flex *:items-center *:justify-center">
-            <CarouselItem className="basis-full h-full pl-0 relative">
+            <CarouselItem className="basis-full h-full pl-0">
               <Image
                 src={img1}
                 alt="img1"
-                className="h-auto w-auto object-cover"
+                className="h-auto w-full object-cover"
               />
-              <button className="absolute bottom-5 right-5 lg:bottom-10 lg:right-10 flex items-center justify-center gap-2 rounded-full bg-white text-black-1 max-sm:text-xs px-4 py-2 lg:px-8 lg:py-3">
-                Shop Now
-                <FaArrowRight />
-              </button>
             </CarouselItem>
             <CarouselItem className="basis-full pl-0 relative">
               <Image
@@ -70,17 +68,15 @@ const Hero = () => {
                 alt="img1"
                 className="h-auto w-full object-top object-cover"
               />
-              <button className="absolute bottom-5 lg:bottom-10 flex items-center justify-center gap-2 rounded-full bg-white text-black-1 max-sm:text-xs px-4 py-2 lg:px-8 lg:py-3">
-                Shop Now
-                <FaArrowRight />
-              </button>
             </CarouselItem>
             <CarouselItem className="basis-full pl-0">
-              <Image
-                src={img3}
-                alt="img1"
-                className="object-cover"
-              />
+              <Image src={img3} alt="img1" className="object-cover" />
+            </CarouselItem>
+            <CarouselItem className="basis-full pl-0">
+              <Image src={img4} alt="img1" className="object-cover" />
+            </CarouselItem>
+            <CarouselItem className="basis-full pl-0">
+              <Image src={img5} alt="img1" className="object-cover" />
             </CarouselItem>
           </CarouselContent>
         </Carousel>
