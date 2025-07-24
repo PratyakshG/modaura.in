a3ba803db785d16eec650c0d0d9a47abecf1d62e
Pratyaksh Gupta <pincodeazm@gmail.com>
2025-07-24 18:04:58 +0530

    changes to order page UI

--- app/(shop)/orders/page.tsx
+++ app/(shop)/orders/page.tsx
@@ -20,12 +20,11 @@ const Page = () => {
     getOrders();
   }, []);
 
-  useEffect(() => {
-    console.log("Orders updated", orders);
-  }, [orders]);
-
   return (
-    <section id="orders" className="w-full">
+    <section
+      id="orders"
+      className="w-full lg:px-10 rounded-xl lg:rounded-4xl"
+    >
       {orders.length === 0 ? (
         <div className="text-center w-full h-full flex flex-col items-center justify-center gap-5 my-5 py-10 shadow-lg border border-neutral-200">
           <div className="flex flex-col items-center justify-center gap-5">
@@ -45,45 +44,77 @@ const Page = () => {
           </Link>
         </div>
       ) : (
-        <div className="w-full space-y-5">
+        <div className="w-full space-y-3 lg:space-y-5">
           <h1 className="text-3xl lg:text-4xl font-medium">Order History</h1>
 
           <ul className="flex flex-col gap-5">
             {orders.map((order) => (
               <div
                 key={order._id.toString()}
-                className="bg-white px-5 py-8 rounded-xl shadow-md flex items-start justify-between"
+                className="rounded-md lg:rounded-xl flex flex-col items-start justify-between border border-neutral-300"
               >
-                <div className="flex flex-col gap-2">
+                <div className="bg-neutral-200 px-3 py-2 lg:px-10 w-full flex flex-wrap items-center justify-between *:text-xs rounded-t-md lg:rounded-t-xl max-md:space-x-5 max-md:space-y-2">
+                  <li>
+                    PAYMENT MODE
+                    <br />
+                    <span className="font-medium text-sm">
+                      {order.paymentMode}
+                    </span>
+                  </li>
+                  <li>
+                    TOTAL
+                    <br />
+                    <span className="font-medium text-sm">
+                      Rs. {order.amount}.00
+                    </span>
+                  </li>
+                  <li>
+                    ORDER PLACED
+                    <br />
+                    <span className="font-medium text-sm">
+                      {new Date(order.createdAt).toLocaleString("en-IN", {
+                        day: "2-digit",
+                        month: "long",
+                        year: "numeric",
+                      })}
+                    </span>
+                  </li>
+                  <li>
+                    ORDER ID
+                    <br />
+                    <span className="font-medium text-sm">
+                      {order._id.toString()}
+                    </span>
+                  </li>
+                </div>
+
+                <div className="flex flex-col gap-2 w-full p-3 lg:px-10 lg:py-5">
                   {order.items.map((item) => (
                     <div key={item._id.toString()} className="flex gap-2">
-                      <Image
-                        src={item.images[0]}
-                        alt="item-image"
-                        height={50}
-                        width={50}
-                        className="rounded"
-                      />
-                      <div>
-                        <li className="font-semibold md:text-lg max-sm:leading-4">{item.name}</li>
-                        <li>quantity : {item.quantity}</li>
+                      <div className="rounded max-sm:size-12 size-24 aspect-square object-cover place-items-center overflow-hidden">
+                        <Image
+                          src={item.images[0]}
+                          alt="item-image"
+                          height={50}
+                          width={50}
+                          className="object-cover size-full scale-125"
+                        />
+                      </div>
+                      <div className="max-sm:space-y-1">
+                        <li className="text-xl max-sm:leading-4 hover:underline text-darkTeal">
+                          <Link href={`/product/${item._id}`}>{item.name}</Link>
+                        </li>
+
+                        <li className="text-xs md:text-sm max-sm:leading-2.5 text-neutral-500">
+                          Category: {item.category}
+                        </li>
+
+                        <li className="text-xs md:text-sm max-sm:leading-2.5 text-neutral-500">
+                          Quantity: {item.quantity}
+                        </li>
                       </div>
                     </div>
                   ))}
-                </div>
-
-                <div className="text-right text-sm md:text-base">
-                  <li>Amount : {order.amount}</li>
-                  <li>Payment Mode : {order.paymentMode}</li>
-                  <li>
-                    Order Date :{" "}
-                    {new Date(order.createdAt).toLocaleString("en-IN", {
-                      day: "2-digit",
-                      month: "2-digit",
-                      year: "numeric",
-                    })}
-                  </li>
-                  <li>Order ID : {order._id.toString()}</li>
                 </div>
               </div>
             ))}
